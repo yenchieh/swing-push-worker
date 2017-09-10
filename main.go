ab198ce63a7a2e10788a9e05c37c613e841e680c
Yen-Chieh Chen <jack08300@gmail.com>
2017-09-10 17:49:25 -0400

    Android push notification

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"net/smtp"
 	"time"
 
+	"github.com/NaySoftware/go-fcm"
 	"github.com/RobotsAndPencils/buford/certificate"
 	"github.com/RobotsAndPencils/buford/payload"
 	"github.com/RobotsAndPencils/buford/push"
@@ -41,6 +42,7 @@ type User struct {
 	FirstName      string
 	LastName       string
 	RegistrationID string
+	AndroidToken   string
 	Lang           string
 }
 
@@ -87,6 +89,12 @@ func main() {
 			Usage:  "Push cert password",
 			Value:  "",
 		},
+		cli.StringFlag{
+			EnvVar: "FCM_SERVER_KEY",
+			Name:   "fcm_server_key",
+			Usage:  "FCM server key",
+			Value:  "",
+		},
 	}
 
 	app.Action = func(c *cli.Context) error {
@@ -96,10 +104,10 @@ func main() {
 			Password: c.String("database_password"),
 			IP:       c.String("database_IP"),
 		}
+		fmt.Println("Push Server Started")
 
-		gocron.Every(30).Seconds().Do(startPushNotification, database, c.String("cert_password"))
+		gocron.Every(30).Seconds().Do(startPushNotification, database, c.String("cert_password"), c.String("fcm_server_key"))
 		<-gocron.Start()
-		//startPushNotification()
 
 		return nil
 	}
@@ -119,15 +127,15 @@ func connectToDatabase(database Database) *sql.DB {
 	return db
 }
 
-func startPushNotification(database Database, certPassword string) {
+func startPushNotification(database Database, certPassword string, serverKey string) {
 	//log.Println("Check the notification task")
-
 	db := connectToDatabase(database)
 	defer db.Close()
 
 	notificationEvent, err := db.Query("SELECT c.id, name, alert, COALESCE(description, '') as description, push_time_utc, user_id, " +
-		"status, email, last_name, first_name, registration_id FROM event c JOIN user u ON c.user_id = u.id " +
-		"WHERE alert >= 32 AND status != 'NOTIFICATION_SENT' AND (`repeat` = '' or `repeat` is null) AND registration_id != '' AND registration_id is not null AND push_time_utc >= now() AND push_time_utc <= now() + INTERVAL 30 SECOND")
+		"status, email, last_name, first_name, registration_id, android_registration_token FROM event c JOIN user u ON c.user_id = u.id " +
+		"WHERE alert >= 32 AND status != 'NOTIFICATION_SENT' AND (`repeat` = '' or `repeat` is null) AND registration_id != '' AND " +
+		"(registration_id is not null OR android_registration_token is not null) AND push_time_utc >= now() AND push_time_utc <= now() + INTERVAL 30 SECOND")
 
 	if err != nil {
 		log.Fatal(err)
@@ -141,7 +149,7 @@ func startPushNotification(database Database, certPassword string) {
 
 		notificationEvent.Scan(&calendarEvent.ID, &calendarEvent.EventName, &calendarEvent.Alert, &calendarEvent.Description,
 			&calendarEvent.PushDate, &calendarEvent.UserId, &calendarEvent.Status, &calendarUser.Email,
-			&calendarUser.FirstName, &calendarUser.LastName, &calendarUser.RegistrationID)
+			&calendarUser.FirstName, &calendarUser.LastName, &calendarUser.RegistrationID, &calendarUser.AndroidToken)
 
 		var notificationData NotificationData
 		notificationData.Event = calendarEvent
@@ -152,8 +160,8 @@ func startPushNotification(database Database, certPassword string) {
 	}
 
 	repeatEvent, err := db.Query("SELECT c.id, name, alert, COALESCE(description, '') as description, DAYNAME(push_time_utc) as weekday, push_time_utc," +
-		" `repeat`, user_id, status, email, last_name, first_name, registration_id FROM event c JOIN user u ON c.user_id = u.id " +
-		"WHERE alert >= 32 AND (`repeat` != '' AND `repeat` is not null) AND registration_id != '' AND registration_id is not null AND " +
+		" `repeat`, user_id, status, email, last_name, first_name, registration_id, android_registration_token FROM event c JOIN user u ON c.user_id = u.id " +
+		"WHERE alert >= 32 AND (`repeat` != '' AND `repeat` is not null) AND registration_id != '' AND (registration_id is not null OR android_registration_token is not null) AND " +
 		"hour(push_time_utc) = hour(now()) AND minute(push_time_utc) = minute(now())")
 
 	if err != nil {
@@ -166,7 +174,7 @@ func startPushNotification(database Database, certPassword string) {
 
 		repeatEvent.Scan(&calendarEvent.ID, &calendarEvent.EventName, &calendarEvent.Alert, &calendarEvent.Description, &calendarEvent.Weekday,
 			&calendarEvent.PushDate, &calendarEvent.Repeat, &calendarEvent.UserId, &calendarEvent.Status, &calendarUser.Email,
-			&calendarUser.FirstName, &calendarUser.LastName, &calendarUser.RegistrationID)
+			&calendarUser.FirstName, &calendarUser.LastName, &calendarUser.RegistrationID, &calendarUser.AndroidToken)
 
 		var notificationData NotificationData
 		notificationData.Event = calendarEvent
@@ -177,12 +185,14 @@ func startPushNotification(database Database, certPassword string) {
 	}
 
 	if len(notificationDatas) > 0 {
-		pushNotification(notificationDatas, certPassword)
+
+		pushNotificationiOS(notificationDatas, certPassword)
+		pushNotificationAndroid(notificationDatas, serverKey)
 	}
 
 }
 
-func pushNotification(notificationDatas []NotificationData, certPassword string) {
+func pushNotificationiOS(notificationDatas []NotificationData, certPassword string) {
 	cert, err := certificate.Load("./cert/com_kd_swing.p12", certPassword)
 	panicError(err)
 
@@ -196,33 +206,56 @@ func pushNotification(notificationDatas []NotificationData, certPassword string)
 	service := push.NewService(client, push.Production)
 
 	for _, notificationData := range notificationDatas {
-		log.Println("------------------------------------")
-		log.Printf("Process: %#v \n", notificationData)
+		if notificationData.User.RegistrationID != "" {
+			log.Println("------------------------------------")
+			log.Printf("Process: %#v \n", notificationData)
 
-		if notificationData.Event.Repeat != "" {
-			if !SendRepeatNotification(notificationData.Event) {
-				log.Println("Weekly event, but it's not today")
-				log.Println("------------------------------------")
-				continue
+			if notificationData.Event.Repeat != "" {
+				if !SendRepeatNotification(notificationData.Event) {
+					log.Println("Weekly event, but it's not today")
+					log.Println("------------------------------------")
+					continue
+				}
+			}
+
+			message := fmt.Sprintf("You have an event: %s", notificationData.Event.EventName)
+
+			p := payload.APS{
+				Alert: payload.Alert{Body: message},
+			}
+
+			b, err := json.Marshal(p)
+			panicError(err)
+
+			id, err := service.Push(notificationData.User.RegistrationID, header, b)
+			panicError(err)
+
+			log.Printf("Pushed to %s\n", id)
+			log.Println("------------------------------------")
+		}
+
+	}
+
+}
+
+func pushNotificationAndroid(notificationDatas []NotificationData, serverKey string) {
+	for _, notificationData := range notificationDatas {
+		if notificationData.User.AndroidToken != "" {
+			data := map[string]string{
+				"message": notificationData.Event.EventName,
+			}
+			c := fcm.NewFcmClient(serverKey)
+			fmt.Println(notificationData.User.AndroidToken)
+			c.NewFcmMsgTo(notificationData.User.AndroidToken, data)
+			status, err := c.Send()
+			if err == nil {
+				status.PrintResults()
+			} else {
+				fmt.Println(err)
 			}
 		}
 
-		message := fmt.Sprintf("You have an event: %s", notificationData.Event.EventName)
-
-		p := payload.APS{
-			Alert: payload.Alert{Body: message},
-		}
-
-		b, err := json.Marshal(p)
-		panicError(err)
-
-		id, err := service.Push(notificationData.User.RegistrationID, header, b)
-		panicError(err)
-
-		log.Printf("Pushed to %s\n", id)
-		log.Println("------------------------------------")
 	}
-
 }
 
 func SendRepeatNotification(event CalendarEvent) bool {
